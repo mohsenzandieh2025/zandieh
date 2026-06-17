772d775075a6243176aac45000f7c7bae019637c
mohsen zandieh <208471245+mohsenzandieh2025@users.noreply.github.com>
2026-06-17 20:16:47 +0330

    Delete assets/js directory

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.mobile-menu')?.addEventListener('click', () => {
-    document.querySelector('.main-nav').classList.toggle('active');
-});
-window.addEventListener('load', () => {
-    document.getElementById('preloader')?.classList.add('fade-out');
-});
